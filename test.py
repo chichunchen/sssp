ddeb6a6b8d94360c5a66ca5236ed7a3d7f34cd5c
Joss Wang <jossome.one@gmail.com>
2017-12-14 17:07:45 -0500

    Add command line args for python test

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import subprocess
+import sys
 
+# print(sys.argv)
+N = int(sys.argv[1])
+d = sys.argv[2]
+S = int(sys.argv[3])
+t = sys.argv[4]
 
-for n in range(8, 20):
-    for d in [10]:
-        for s in range(100):
-            res = subprocess.run(['java', 'SSSP', '-a', '1', '-t', '4', '-n', str(n), '-d', str(d), '-s', str(s)], stdout = subprocess.PIPE)
-            res_t = subprocess.run(['java', 'SSSP', '-a', '1', '-t', '4', '-n', str(n), '-d', str(d), '-t', '4', '-s', str(s)], stdout = subprocess.PIPE)
-            if len(str(res.stdout).split('\n')) != len(str(res_t.stdout).split('\n')):
-                print("Error at", n, t, s)
-            else:
-                print("Success at", n, s)
+for n in range(N, N+60):
+    for s in range(S, S+60):
+        res = subprocess.run(['java', 'SSSP', '-a', '1', '-t', t, '-n', str(n), '-d', d, '-s', str(s)], stdout = subprocess.PIPE)
+        res_t = subprocess.run(['java', 'SSSP', '-a', '1', '-t', t, '-n', str(n), '-d', d, '-t', '4', '-s', str(s)], stdout = subprocess.PIPE)
+        if len(str(res.stdout).split('\n')) != len(str(res_t.stdout).split('\n')):
+            print("Error at", n, t, s)
+        else:
+            print("Success at", n, s)
