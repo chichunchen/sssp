6960217c9059e8df8b2d2934685f5c1b1e2536df
Joss <jossome.one@gmail.com>
2017-12-13 23:27:21 -0500

    python script tester

---
+++ test.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+for n in range(6, 20):
+    for d in [10]:
+        for s in range(100):
+            res = subprocess.run(['java', 'SSSP', '-a', '1', '-n', str(n), '-d', str(d), '-s', str(s)], stdout = subprocess.PIPE)
+            res_t = subprocess.run(['java', 'SSSP', '-a', '1', '-n', str(n), '-d', str(d), '-t', '4', '-s', str(s)], stdout = subprocess.PIPE)
+            if len(str(res.stdout).split('\n')) != len(str(res_t.stdout).split('\n')):
+                print("Error at", n, t, s)
+
